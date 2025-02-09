0903d73a10de708242f48e6d50a40ff305968439
Anastasia Krutikova <anastasiakrut@mail.ru>
2025-02-09 16:36:22 +0300

    Homework_6 done

---
+++ homework/Anastasia_Krutikova/Homework_6/exercise_1.py
@@ -0,0 +1,23 @@
+#  Задание 1
+
+text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel.'
+        ' Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
+
+text = text.split()
+#print(text)
+
+new_text = []
+for word in text:
+    if word.endswith('.'):
+        index_point = (word.index('.'))
+        new_word_p = word[:index_point] + 'ing' + '.'
+        new_text.append(new_word_p)
+    elif word.endswith(','):
+        index_comma = (word.index(','))
+        new_word_c = word[:index_comma] + 'ing' + ","
+        new_text.append(new_word_c)
+    else:
+        new_word = word + 'ing'
+        new_text.append(new_word)
+
+print(' '.join(new_text))
