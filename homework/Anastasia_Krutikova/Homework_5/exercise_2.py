0c71b4b3efc9de59fde710136307a08cbbaf6452
Anastasia Krutikova <anastasiakrut@mail.ru>
2025-02-09 12:31:30 +0300

    fixed exercise_2 and left one var in exercise_3

--- homework/Anastasia_Krutikova/Homework_5/exercise_2.py
+++ homework/Anastasia_Krutikova/Homework_5/exercise_2.py
@@ -7,14 +7,14 @@ result_3 = 'результат работы программы: 9'
 # result_1_new = int(result_1.split()[2]) + 10
 # print(result_1_new)
 
-index_result_1 = (result_1.index('42'))
-sum_result_1 = int((result_1[index_result_1:])) + 10
-print(sum_result_1)
+index_result_1 = (result_1.index(': '))
+print(int(result_1[index_result_1+1:]) + 10)
 
-index_result_2 = (result_2.index('514'))
-sum_result_2 = int((result_2[index_result_2:])) + 10
-print(sum_result_2)
+index_result_2 = (result_2.index(': '))
+print(int(result_2[index_result_2+1:]) + 10)
 
-index_result_3 = (result_3.index('9'))
-sum_result_3 = int((result_3[index_result_3:])) + 10
-print(sum_result_3)
+index_result_3 = (result_3.index(': '))
+print(int(result_3[index_result_3+1:]) + 10)
+
+
+
--- homework/Anastasia_Krutikova/Homework_5/exercise_3.py
+++ homework/Anastasia_Krutikova/Homework_5/exercise_3.py
@@ -6,10 +6,4 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 
-name_1, name_2, name_3 = students
-math, biology, geography = subjects
-
-print(f"Students {name_1}, {name_2}, {name_3} study these subjects: {math}, {biology}, {geography}")
-
-#  var_2
 print(f"Students {', '.join(students)} study these subjects: {', '.join(subjects)}")
