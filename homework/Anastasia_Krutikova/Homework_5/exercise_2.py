9b133c69c7e621a5cc3d49c39fc9f3c399887518
Anastasia Krutikova <anastasiakrut@mail.ru>
2025-02-09 12:34:58 +0300

    fixed exercise_2

--- homework/Anastasia_Krutikova/Homework_5/exercise_2.py
+++ homework/Anastasia_Krutikova/Homework_5/exercise_2.py
@@ -8,13 +8,10 @@ result_3 = 'результат работы программы: 9'
 # print(result_1_new)
 
 index_result_1 = (result_1.index(': '))
-print(int(result_1[index_result_1+1:]) + 10)
+print(int(result_1[index_result_1 + 1:]) + 10)
 
 index_result_2 = (result_2.index(': '))
-print(int(result_2[index_result_2+1:]) + 10)
+print(int(result_2[index_result_2 + 1:]) + 10)
 
 index_result_3 = (result_3.index(': '))
-print(int(result_3[index_result_3+1:]) + 10)
-
-
-
+print(int(result_3[index_result_3 + 1:]) + 10)
