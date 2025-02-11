f95a8dd7ac4e400ff938407f12521a016042bb73
Anastasia Krutikova <anastasiakrut@mail.ru>
2025-02-11 22:22:25 +0300

    Homework_7 done

--- homework/Anastasia_Krutikova/Homework_7/exercise_3.py
+++ homework/Anastasia_Krutikova/Homework_7/exercise_3.py
@@ -5,6 +5,7 @@ def result_example(number_outside):
     sum_result = int((number_outside[index_result + 1:])) + 10
     return sum_result
 
+
 print(result_example('результат операции: 42'))
 print(result_example('результат операции: 54'))
 print(result_example('результат работы программы: 209'))
