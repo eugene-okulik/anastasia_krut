f2b85993bf99de82a0bb1df16757b1f1a99470f3
Anastasia Krutikova <anastasiakrut@mail.ru>
2025-02-21 20:41:49 +0300

    Homework_9 done

--- homework/Anastasia_Krutikova/Homework_9/exercise_1.py
+++ homework/Anastasia_Krutikova/Homework_9/exercise_1.py
@@ -3,7 +3,7 @@ import datetime
 
 my_data = 'Jan 15, 2023 - 12:05:33'
 
-python_date = datetime.datetime.strptime(my_data, '%b %d, %Y - %X' )
+python_date = datetime.datetime.strptime(my_data, '%b %d, %Y - %X')
 # print(python_date)
 # 15.01.2023, 12:05
 print((python_date.strftime('%B %d, %Y - %X')).split()[0])
