5890a5dfeb77b02b5279d6846c759931796c455c
Anastasia Krutikova <anastasiakrut@mail.ru>
2025-02-21 13:02:04 +0300

    Homework_8 done

---
+++ homework/Anastasia_Krutikova/Homework_8/exercise_1.py
@@ -0,0 +1,19 @@
+#  Задание_1
+
+import random
+
+
+def total_price():
+    salary = int(input("Введите цену: "))
+    bonus = random.choice([True, False])
+
+    if bonus:
+        bonus_random = random.randint(1,1000)
+        price = salary + bonus_random
+    else:
+        price = salary
+
+    print(f"{salary}, {bonus} - '${price}'")
+
+total_price()
+
---
+++ homework/Anastasia_Krutikova/Homework_8/exercise_2.py
@@ -0,0 +1,24 @@
+#  Задание_2
+
+def fib(limit=100000000):
+    number_1 = 0
+    number_2 = 1
+    f_count = 0
+    while f_count < limit:
+        yield number_1
+        number_1, number_2 = number_2, number_1 + number_2
+        f_count += 1
+
+count = 0
+
+for number in fib(1000000):
+    count += 1
+    if count == 5:
+        print(number)
+    elif count == 200:
+        print(number)
+    elif count == 1000:
+        print(number)
+    elif count == 100000:
+        print(number)
+
